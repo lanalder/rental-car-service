cdd19d4d31ecb77a5bbc0021058b2d1056c7dfee
lanalder <lalanalder@gmail.com>
2021-06-02 11:54:22 +1200

    fickle

--- js/custom.js
+++ js/custom.js
@@ -3,11 +3,29 @@
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
 
+  var dm;
+
+  function initDim() {
+    dm = [window.innerWidth, window.innerHeight];
+    document.body.style.width = dm[0]+'px';
+    document.body.style.height = dm[1]+'px';
+    canvas.style.width = dm[0]+'px';
+    canvas.style.height = dm[1]+'px';
+    roadscale(dm);
+  }
+
   ctx.beginPath();
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 700, 400, 200);
 
+  window.addEventListener('load', function() {
+    initDim();
+  })
 
-
+  function roadscale(currentClientDim) {
+    ctx.fillStyle = 'black';
+    //getting some nice ratios or whatever
+    let frac = Math.floor(dm[1]/5);
+    console.log(0, dm[1]-frac, dm[0], frac);
+    ctx.fillRect(0, dm[1]-frac, dm[0], frac);
+  }
 
 }());
