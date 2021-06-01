f98822a24d5d66b554919edb20bc60767e3b9420
lanalder <lalanalder@gmail.com>
2021-06-02 11:26:21 +1200

    1st

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+(function () {
+
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  ctx.beginPath();
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 700, 400, 200);
+
+
+
+
+}());
