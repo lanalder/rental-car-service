dea4d36bbeb586ec721aa95175cd663d994dd211
lanalder <lalanalder@gmail.com>
2021-06-03 07:12:55 +1200

    julie n spike dkes on a bike

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 (function () {
 
+  var dm = [];
+
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
-
-  var dm;
 
   function initDim() {
     dm = [window.innerWidth, window.innerHeight];
@@ -11,20 +11,20 @@
     document.body.style.height = dm[1]+'px';
     canvas.style.width = dm[0]+'px';
     canvas.style.height = dm[1]+'px';
-    roadscale(dm);
+    roadscale();
   }
 
   ctx.beginPath();
+  ctx.fillStyle = 'black';
 
   window.addEventListener('load', function() {
     initDim();
   })
 
-  function roadscale(currentClientDim) {
-    ctx.fillStyle = 'black';
+  function roadscale() {
     //getting some nice ratios or whatever
     let frac = Math.floor(dm[1]/5);
-    console.log(0, dm[1]-frac, dm[0], frac);
+    console.log(0, dm[1]-Math.floor(dm[1]/5), dm[0], Math.floor(dm[1]/5));
     ctx.fillRect(0, dm[1]-frac, dm[0], frac);
   }
 
