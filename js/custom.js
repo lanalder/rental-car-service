c251542251cb3b6cd51b1393785a7231279bb36e
lanalder <lalanalder@gmail.com>
2021-06-03 12:24:50 +1200

    whoosh

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,48 @@
 (function () {
 
-  var dm = [];
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+ctx.beginPath();
 
-  function initDim() {
-    dm = [window.innerWidth, window.innerHeight];
-    document.body.style.width = dm[0]+'px';
-    document.body.style.height = dm[1]+'px';
-    canvas.style.width = dm[0]+'px';
-    canvas.style.height = dm[1]+'px';
-    roadscale();
+let init = {
+  get w() { return window.innerWidth },
+  get h() { return window.innerHeight },
+  scale() {
+    document.body.style.width = `${this.w}px`;
+    document.body.style.height = `${this.h}px`;
+    canvas.style.width = `${this.w}px`;
+    canvas.style.height = `${this.h}px`;
   }
+}
 
-  ctx.beginPath();
-  ctx.fillStyle = 'black';
+let road = {
+  // everything a responsive ratio since maths is marginally (marginally!) more interesting than css
+  get h() { return Math.ceil(canvas.height/3) },
+  get y() { return Math.ceil(canvas.height-this.h*1.3) },
+  // for lines
+  get linY() { return this.y+(Math.ceil(this.y/5)) },
+  get linW() { return Math.ceil(init.w/10) },
+  get linX() { return },
 
-  window.addEventListener('load', function() {
-    initDim();
-  })
-
-  function roadscale() {
-    //getting some nice ratios or whatever
-    let frac = Math.floor(dm[1]/5);
-    console.log(0, dm[1]-Math.floor(dm[1]/5), dm[0], Math.floor(dm[1]/5));
-    ctx.fillRect(0, dm[1]-frac, dm[0], frac);
+  draw() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, this.y, init.w, this.h);
+    for (let i = 0; i < Math.ceil(init.w/5); i++) {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(i, this.linY, this.linW, Math.ceil(this.linW/36));
+    }
   }
+}
+
+init.scale();
+road.draw();
+
+window.onresize = resize;
+
+function resize() {
+  init.scale();
+  road.draw();
+}
 
 }());
