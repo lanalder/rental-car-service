097d59a981118ca7ca1a7d701df76248343c4d0b
lanalder <lalanalder@gmail.com>
2021-06-03 21:58:36 +1200

    canvas is a canvas on a canvas diduknw

--- js/custom.js
+++ js/custom.js
@@ -3,46 +3,68 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-
 let init = {
   get w() { return window.innerWidth },
   get h() { return window.innerHeight },
-  scale() {
+  setScl() {
     document.body.style.width = `${this.w}px`;
     document.body.style.height = `${this.h}px`;
-    canvas.style.width = `${this.w}px`;
-    canvas.style.height = `${this.h}px`;
+    canvas.width = this.w;
+    canvas.height = this.h;
   }
 }
 
+ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+// cubic-bezier(.17,.67,.83,.67)
+
 let road = {
-  // everything a responsive ratio since maths is marginally (marginally!) more interesting than css
-  get h() { return Math.ceil(canvas.height/3) },
-  get y() { return Math.ceil(canvas.height-this.h*1.3) },
-  // for lines
-  get linY() { return this.y+(Math.ceil(this.y/5)) },
-  get linW() { return Math.ceil(init.w/10) },
-  get linX() { return },
+  h: init.h/6,
+  get y() { return init.h-this.h*1.5 },
+
+  // for lines... new fav pattern ig isget (n 3s today) nice 2 kno what ur up to tho instead of giganturan expressions (statements??) n mostly here bc objs suck at self-awareness (no self-ref this)
+  get linY() { return this.y+(this.h/3) },
+  get linH() { return this.h/9 },
+  linW: init.w/9,
 
   draw() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, this.y, init.w, this.h);
-    for (let i = 0; i < Math.ceil(init.w/5); i++) {
-      ctx.fillStyle = 'white';
-      ctx.fillRect(i, this.linY, this.linW, Math.ceil(this.linW/36));
-    }
+
+    let gap = this.linW/1.5;
+    // ctx.beginPath();
+
+    // for (let i = 0; i < 12; i++) {
+
+
+      // ctx.moveTo(gap*i+10, this.linY);
+      // ctx.lineTo((gap*i-10)+this.linW, this.linY);
+      // ctx.lineTo(gap*i+this.linW, this.linY+this.linH);
+      // ctx.lineTo(gap*i, this.linY+this.linH);
+      // ctx.closePath();
+      // ctx.fillStyle = 'white';
+      // ctx.fillRect(gap*i, this.linY, this.linW, this.linH);
+      // gap = this.linW+(this.linW/1.5);
+    // }
   }
 }
 
-init.scale();
+init.setScl();
 road.draw();
 
 window.onresize = resize;
 
 function resize() {
-  init.scale();
+  init.setScl();
   road.draw();
 }
+
+
+ctx.strokeStyle = 'red';
+ctx.beginPath();
+ctx.moveTo(10, 10);
+ctx.lineTo(190, 10);
+ctx.lineTo(200, 110);
+ctx.lineTo(0, 110);
+ctx.closePath();
 
 }());
