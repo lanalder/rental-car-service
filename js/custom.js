520b926375477fa99b8357a2d52dc753bfa15bc8
lanalder <lalanalder@gmail.com>
2021-06-08 08:04:56 +1200

    dont miss the train

--- js/custom.js
+++ js/custom.js
@@ -1,82 +1,105 @@
 (function () {
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
 
-ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+  ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
 
-let init = {
-  get w() { return window.innerWidth },
-  get h() { return window.innerHeight },
-  setScl() {
-    document.body.style.width = `${this.w}px`;
-    document.body.style.height = `${this.h}px`;
-    canvas.width = this.w;
-    canvas.height = this.h;
+  let init = {
+    get w() { return window.innerWidth },
+    get h() { return window.innerHeight },
+    setScl() {
+      document.body.style.width = `${this.w}px`;
+      document.body.style.height = `${this.h}px`;
+      canvas.width = this.w;
+      canvas.height = this.h;
+    }
   }
-}
 
-let road = {
-  h: inProptn(1, 6),
-  get y() { return init.h-this.h*1.5 },
-  // for lines... new fav pattern ig isget (n 3s today) nice 2 kno what ur up to tho instead of giganturan expressions (statements??) n mostly here bc objs suck at self-awareness (no self-ref this)
-  get linY() { return this.y+(this.h/3) },
-  get linH() { return this.h/9 },
-  linW: inProptn(0, 9),
-  draw() {
-    // curbs 1st easiest 2 just have behind
-    let shades = ['#9ca297', '#b1bab0'];
-    for (let i = 0; i < 2; i++) {
-      ctx.fillStyle = shades[i];
-      if (i) {
-        ctx.translate(0, 3.3);
+  let road = {
+    h: inProptn(1, 6),
+    get y() { return init.h - this.h * 1.5 },
+    // for lines... new fav pattern ig isget (n 3s today) nice 2 kno what ur up to tho instead of giganturan expressions (statements??) n mostly here bc objs suck at self-awareness (no self-ref this)
+    get linY() { return this.y + (this.h / 3) },
+    get linH() { return this.h / 9 },
+    linW: inProptn(0, 9),
+    draw() {
+      // curbs 1st easiest 2 just have behind
+      let shades = ['#9ca297', '#b1bab0'];
+      for (let i = 0; i < 2; i++) {
+        ctx.fillStyle = shades[i];
+        if (i) {
+          ctx.translate(0, 3.3);
+        }
+        ctx.fillRect(0, this.y - (this.linH / 3) * 1.5, init.w, this.h + (this.linH / 3) * (3 - i * 1.5));
       }
-      ctx.fillRect(0, this.y-(this.linH/3)*1.5, init.w, this.h+(this.linH/3)*(3-i*1.5));
-    }
-    ctx.translate(0, -3.3);
-    // road itself
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, this.y, init.w, this.h);
-    // road markings
-    let gap = this.linW/3;
-    for (let i = 0; i < 18; i++) {
-      ctx.beginPath();
-      ctx.fillStyle = 'white';
-      ctx.moveTo(gap*i+6, this.linY);
-      ctx.lineTo(gap*i+3+this.linW, this.linY);
-      ctx.lineTo(gap*i+this.linW, this.linY+this.linH);
-      ctx.lineTo(gap*i+1.5, this.linY+this.linH);
-      ctx.closePath();
-      ctx.fill();
-      gap = this.linW+(this.linW/1.5);
+      ctx.translate(0, -3.3);
+      // road itself
+      ctx.fillStyle = 'black';
+      ctx.fillRect(0, this.y, init.w, this.h);
+      // road markings
+      let gap = this.linW / 3;
+      for (let i = 0; i < 18; i++) {
+        ctx.beginPath();
+        ctx.fillStyle = 'white';
+        ctx.moveTo(gap * i + 6, this.linY);
+        ctx.lineTo(gap * i + 3 + this.linW, this.linY);
+        ctx.lineTo(gap * i + this.linW, this.linY + this.linH);
+        ctx.lineTo(gap * i + 1.5, this.linY + this.linH);
+        ctx.closePath();
+        ctx.fill();
+        gap = this.linW + (this.linW / 1.5);
+      }
+      grass();
     }
   }
-}
 
-init.setScl();
-road.draw();
+  function grass() {
+    //mayb better 4 reponsiveness as canv drawImag
+    let amt = inProptn(0, 50);
+    for (let i = 0; i < 3; i++) {
+      let roots = [];
+      let soil = document.querySelector(`.grass${i}`);
+      soil.style.width = `${amt * init.w}px`;
+      console.log(soil.width);
+      for (let i = 0; i < amt; i++) {
+        roots[i] = new Image(50, 50);
+        roots[i].src = '../img/grs.png';
+        soil.appendChild(roots[i]);
+      }
+      if (i < 2) {
+        soil.style.bottom = `${(-i) * 30}px`;
+        soil.style.zIndex = i;
+      } else {
+        soil.style.top = `${road.y - 50 - (road.linH / 3)}px`;
+        console.log(road.y);
+      }
+    }
+  }
 
-window.onresize = resize;
+  'load resize'.split(' ').forEach(function(e) {
+    window.addEventListener(e, draw2suit, false);
+  })
 
-function resize() {
-  init.setScl();
-  road.draw();
-}
+  function draw2suit() {
+    init.setScl();
+    road.draw();
+  }
 
-function inProptn(nu, de) {
-  // f 4 width, since usually 0 in arr; t 4 h
-  return !nu ? Math.ceil(Math.abs(init.w/de))
-            : Math.ceil(Math.abs(init.h/de));
-}
+  function inProptn(nu, de) {
+    // f 4 width, since usually 0 in arr; t 4 h
+    return !nu ? Math.ceil(Math.abs(init.w / de))
+              : Math.ceil(Math.abs(init.h / de));
+  }
 
-const ppl = document.querySelector('.ppl-img-cont');
+  const ppl = document.querySelector('.ppl-img-cont');
 
-for (let i = 0; i < ppl.children.length; i++) {
-  ppl.children[i].addEventListener('click', function() {
-    ppl.children[i].classList.toggle('hide');
-    ppl.children[i+1].classList.toggle('hide');
+  for (let i = 0; i < ppl.children.length; i++) {
+    ppl.children[i].addEventListener('click', function() {
+      ppl.children[i].classList.toggle('hide');
+      ppl.children[i + 1].classList.toggle('hide');
 
-  }, false);
-}
+    }, false);
+  }
 
 }());
