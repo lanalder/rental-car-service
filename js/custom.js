e7abe54028fd864f86261e23afece44cf1f33e8b
lanalder <lalanalder@gmail.com>
2021-06-16 10:34:41 +1200

    fire!

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 import Litepicker from 'litepicker';
-import mapboxgl from 'mapbox-gl';
+// import mapboxgl from 'mapbox-gl';
 // import needs to be outside iffe wrapper, otherwise throws undefined errors; i think this is because es6 modules are pre-parsed instead of commonjs (require) being called on demand, meaning that when first read the objs requiring litepicker are not yet defined... obvs out here litepicker-depending objs are still not defined, though they are within an as of yet anon func, so it must be able to access necessary values as returns once wrapper iffe is called, whereas if they were in the same global scope, the module isn't gonna wait for returns but expects those values there from the start
 
 (function () {
@@ -19,12 +19,14 @@ import mapboxgl from 'mapbox-gl';
   let init = {
     get w() { return window.innerWidth; },
     get h() { return window.innerHeight; },
+    // getter used so that page is sorta reponsive w/o css, & so coords of elements are relative to user and not actual page position
 
     setScl() {
       document.body.style.width = `${this.w}px`;
       document.body.style.height = `${this.h}px`;
       canvas.width = this.w;
       canvas.height = this.h;
+      // when init is called, sets off a domino effect of all the other objs that need 2 be initd
       road.draw();
     }
   };
@@ -32,73 +34,83 @@ import mapboxgl from 'mapbox-gl';
   let ppl = {
     cont: document.querySelector('.ppl-cont'),
     picker: ['julie', 'spike'],
+    // names of the little ppl, so that they can be subbed into img src
     chrs: new Array(6),
 
     draw() {
       while (this.cont.lastChild) {
+        // so that on resize, last little ppl can be whooshed away n new ones made
         this.cont.lastChild.remove();
       }
       for (let i = 0; i < 6; i++) {
         this.chrs[i] = new Image();
         this.chrs[i].src = `../img/${this.picker[i%2]}.png`;
         clickable(this.chrs[i]);
+        // adds click events 2 them
         this.cont.appendChild(this.chrs[i]);
       }
       signs.draw();
+      // next obj init
     },
     no: 0
+    // no. ppl clicked, updated in clickable(), important value 2 get later
   };
 
   let road = {
     h: inProptn(1, 6),
+    // this is a mess, inProptn() used often 2 round ratios of window size
     get y() { return init.h - this.h * 1.5; },
-    // for lines... new fav pattern ig isget (n 3s today) nice 2 kno what ur up to tho instead of giganturan expressions (statements??) n mostly here bc objs suck at self-awareness (no self-ref this)
+    // for road positioning
     get linY() { return this.y + (this.h / 3); },
     get linH() { return this.h / 9; },
+    // lin stuff for road marking positioning
     linW: inProptn(0, 9),
-    gaplog: [new Array(2)],
     animark: 0,
+    // this is for the anime func, the value is changed 2 whatever it's set at over there, which is equivalent as you'll see below 2 a translateX value
 
     draw() {
-      // curbs 1st easiest 2 just have behind
+      // drawing curbs first, not that u can really see them
       let shades = ['#9ca297', '#b1bab0'];
       for (let i = 0; i < 2; i++) {
         ctx.fillStyle = shades[i];
         if (i) {
           ctx.translate(0, 3.3);
+          // curbs r actually just 2 slightly different coloured rects behind the road, translate gives some very slight illusion of 3d, slightly
         }
         ctx.fillRect(0, this.y - (this.linH / 3) * 1.5, init.w, this.h + (this.linH / 3) * (3 - i * 1.5));
       }
       ctx.translate(0, -3.3);
+
       // road itself
       ctx.fillStyle = 'black';
       ctx.fillRect(0, this.y, init.w, this.h);
-      // this.markings(0);
     },
+
     markings() {
       // road markings
       let gap = this.linW / 3;
-      this.gaplog[0] = gap;
+
       for (let i = 0; i < 18; i++) {
-        // gotta (or dont gotta but its the lazy way) make sm lines 4 responsiveness, the devil
+        // 18 is arbitrary but big enough to encompass all 'pages' as well as if site is zoomed out
         ctx.beginPath();
         ctx.fillStyle = 'white';
         ctx.moveTo(gap * i + 6 - this.animark, this.linY);
+        // animark is for animate, the essentially translateX val
         ctx.lineTo(gap * i + 3 + this.linW - this.animark, this.linY);
         ctx.lineTo(gap * i + this.linW - this.animark, this.linY + this.linH);
         ctx.lineTo(gap * i + 1.5 - this.animark, this.linY + this.linH);
         ctx.closePath();
         ctx.fill();
+
         gap = this.linW + (this.linW / 1.5);
-        // not simply gap+= this.linW since don't wanna move it along exponentially, gap isn't simply x coord but the offset 4 x (rly only needs 2 be calcd once, but objs get weird if i try set gap vals stored in markStrg at beginning, idk seems some limit 2 self-reference - it sets it as the func and not the returned val)
+        // first gap val is just the offset from x=0, after that offset from last line needs 2 include the prev line width
       }
-      this.gaplog[1] = gap;
       ppl.draw();
     }
   };
 
   let signs = {
-    page: 0,
+    page: 1,
     pS: [document.querySelector('.page-sign-next'), document.querySelector('.page-sign-prev')],
     // height is not responsive atm!!
 
@@ -111,6 +123,7 @@ import mapboxgl from 'mapbox-gl';
       ctx.fillRect(init.w - inProptn(0, 21), this.yPos, inProptn(0, 150), inProptn(1, 6));
       this.pS[0].style.top = `${this.yPos - this.pS[0].clientHeight}px`;
       this.pS[0].style.left = `${init.w - inProptn(0, 21) - this.pS[0].clientWidth}px`;
+      // next obj init
       car.position();
     }
   };
@@ -125,8 +138,11 @@ import mapboxgl from 'mapbox-gl';
 
   let grass = {
     amt: inProptn(0, 50),
+    // each grass img is 50x50 so the amt needed 2 fill up screen is w/50
     roots: new Array(inProptn(0, 50)),
+    // for holding each img
     soil: new Array(4),
+    // for each line of grass, differently positioned
 
     get lawnmower() {
       Array.from(document.querySelector('.grass-cont').children); },
@@ -167,7 +183,8 @@ import mapboxgl from 'mapbox-gl';
     road.markings();
     grass.draw();
     date.init();
-    geo.init();
+    // geo.init();
+    carpark.init();
   }
 
   function inProptn(nu, de) {
@@ -201,23 +218,22 @@ import mapboxgl from 'mapbox-gl';
   signs.pS[0].addEventListener('click', animate, false);
 
   function animate() {
-    // mayb look at giving these anime objs names, easier 2 call prolly better practice mayb but prolly not the issue in repeating them?
-    const c = anime({
+    anime({
       targets: car.thingItself,
-      translateX: 80,
+      translateX: 80 * signs.page,
       // delay: anime.stagger(1000),
       easing: 'easeOutExpo',
       duration: 3000
     });
-    const ib = anime({
+    anime({
       targets: inpBits,
-      translateX: -700,
+      translateX: -(900 * signs.page + signs.page * 100),
       easing: 'easeOutQuad',
       duration: 1500
     });
-    const r = anime({
+    anime({
       targets: road,
-      animark: 800,
+      animark: 800 * signs.page,
       round: 1,
       easing: 'easeOutExpo',
       duration: 2750,
@@ -236,9 +252,9 @@ import mapboxgl from 'mapbox-gl';
     // could follow pattern as in aniroad if more elegant, rn cbf
     let lawn = document.querySelector('.grass-cont');
     grass.draw();
-    const g = anime({
+    anime({
       targets: grass.soil,
-      translateX: -800,
+      translateX: -(800 * signs.page),
       easing: 'easeOutExpo',
       duration: 2750
     });
@@ -300,25 +316,46 @@ import mapboxgl from 'mapbox-gl';
     }
   }
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoibGFuYWxkZXIiLCJhIjoiY2tweGlqd2RmMWVyajJ2b2lrejYzbDZ5diJ9.ELtetZkKKBOunIgDPByWYQ';
 
-  const map = new mapboxgl.Map({
-    container: document.querySelector('.map'),
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-41, 74],
-    zoom: 9
-  });
-
-  let geo = {
-    cont: document.querySelector('.map'),
-    outer: document.querySelector('.map-cont'),
+  let carpark = {
+    // fractions man >:(
     init() {
-      this.cont.style.width = `${inProptn(0, 3)}px`;
-      this.cont.style.height = `${inProptn(1, 5)}px`;
-      this.outer.style.left = `${init.w * 2}px`;
+      ctx.beginPath();
+      ctx.fillStyle = 'black';
+      ctx.moveTo(init.w / 2, init.h);
+      ctx.lineTo(init.w / 2 + init.w / 5, init.h / 2);
+      ctx.lineTo(init.w, init.h / 2);
+      ctx.lineTo(init.w, init.h);
+      ctx.closePath();
+      ctx.fill();
 
+      console.log(grass.lawnmower);
     }
   };
+
+
+
+  // mapboxgl.accessToken = 'pk.eyJ1IjoibGFuYWxkZXIiLCJhIjoiY2tweGlqd2RmMWVyajJ2b2lrejYzbDZ5diJ9.ELtetZkKKBOunIgDPByWYQ';
+  //
+  // const map = new mapboxgl.Map({
+  //   container: document.querySelector('.map'),
+  //   style: 'mapbox://styles/mapbox/streets-v11',
+  //   center: [-74.5, 40],
+  //   zoom: 9
+  // });
+  //
+  // let geo = {
+  //   cont: document.querySelector('.map'),
+  //   init() {
+  //     this.cont.style.width = `${inProptn(0, 3)}px`;
+  //     this.cont.style.height = `${inProptn(1, 3)}px`;
+  //     document.querySelector('.map-cont').style.left = `${init.w * 2}px`;
+  //   }
+  // };
+  //
+  // let geoInfo = {
+  //
+  // }
 
 
 
