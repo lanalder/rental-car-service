29736c8d7dee152e6d873a33602288390e967a07
lanalder <lalanalder@gmail.com>
2021-06-19 16:36:10 +1200

    day done

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,7 @@
+// import $ from 'jquery';
+//   window.jQuery = $;
+//   window.$ = $;
+
 import Litepicker from 'litepicker';
 // import mapboxgl from 'mapbox-gl';
 // import needs to be outside iffe wrapper, otherwise throws undefined errors; i think this is because es6 modules are pre-parsed instead of commonjs (require) being called on demand, meaning that when first read the objs requiring litepicker are not yet defined... obvs out here litepicker-depending objs are still not defined, though they are within an as of yet anon func, so it must be able to access necessary values as returns once wrapper iffe is called, whereas if they were in the same global scope, the module isn't gonna wait for returns but expects those values there from the start
@@ -6,32 +10,18 @@ import Litepicker from 'litepicker';
 
   const anime = require('animejs/lib/anime.js');
   // const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+  // const parsley = require('parsleyjs/dist/parsley.min.js');
 
+  // obj to contain the transport dataset
   const omni = {
-    mb: new Map([
-      ['ppl', [1, 1]],
-      ['days', [1, 5]],
-      ['$', 109],
-      ['gas', 3.7]
-    ]),
-    mn: new Map([
-      ['ppl', [1, 2]],
-      ['days', [1, 10]],
-      ['$', 129],
-      ['gas', 8.5]
-    ]),
-    must: new Map([
-      ['ppl', [1, 5]],
-      ['days', [3, 10]],
-      ['$', 144],
-      ['gas', 9.7]
-    ]),
-    cpv: new Map([
-      ['ppl', [2, 6]],
-      ['days', [2, 15]],
-      ['$', 200],
-      ['gas', 17]
-    ])
+    keyz: ['mb', 'mn', 'must', 'cpv'],
+    // that which pairs car with vals below are the indices of above arr
+    vals: {
+      ppl: [[1, 1], [1, 2], [1, 5], [2, 6]],
+      days: [[1, 5], [1, 10], [3, 10], [2, 15]],
+      coin: [109, 129, 144, 200],
+      gas: [109, 129, 144, 200]
+    }
   };
 
   const canvas = document.getElementById('canvas');
@@ -43,22 +33,14 @@ import Litepicker from 'litepicker';
   var inpBits = Array.from(document.querySelectorAll('.inp-el'));
   // all the input-taking elements so can carousel them round n treat their values appropriately
 
-  const picker = new Litepicker({
-    element: document.querySelector('.date'),
-    startDate: new Date(),
-    inlineMode: true,
-    autoRefresh: true,
-    autoApply: true
-  });
-
   let init = {
     get w() { return window.innerWidth; },
     get h() { return window.innerHeight; },
-    // getter used so that page is sorta reponsive w/o css, & so coords of elements are relative to user and not actual page position
+    // getter used so that page is sorta reponsive w/o css, & so coords of elements are relative to user and not actual page position (ideally)
     setScl() {
       document.body.style.width = `${this.w}px`;
       document.body.style.height = `${this.h}px`;
-      // style used on html els and not for canvas, since canvas a js obj which doesn't read/need css
+      // style used on html els and not for canvas, since canvas css doesn't determine actual available coords, but as an obj its size does this as well as set automatically size as an element (im p sure)
       canvas.width = this.w;
       canvas.height = this.h;
       // when init is called, sets off a domino effect of all the other objs that need 2 be initd
@@ -66,13 +48,15 @@ import Litepicker from 'litepicker';
     }
   };
 
+  // _*_*_*_*_*_*_*_*_| INITIAL OBJS |_*_*_*_*_*_*_*_*_*_
+
   let road = {
-    // these getters are for the more complex calcs so the code below isn't an instance migraine, i mean it's still messy but road's h always inProtn(1, 7) and the line w inProtn(0, 9)
+    // these getters (methods ig) are for the more complex calcs so the code below isn't an instant migraine, i mean it's still messy but road's h always inProtn(1, 7) and the line w inProtn(0, 9)
     get y() { return init.h - inProptn(1, 7) * 1.5; },
     get linY() { return this.y + (inProptn(1, 7) / 3); },
     get linH() { return inProptn(1, 7) / 12; },
     animark: 0,
-    // this is for the anime func, the value is changed 2 whatever it's set at over there, which is equivalent as you'll see below 2 a translateX value
+    // this is for the anime func, the value is changed 2 whatever it's set at over there, which is equivalent as you'll see below 2 a translateX value (which can't simply b used bc of canvas, don't wanna move the whole thing but only some of its drawings)
     draw() {
       // drawing curbs first
       let shades = ['#9ca297', '#b1bab0'];
@@ -97,7 +81,6 @@ import Litepicker from 'litepicker';
         ctx.beginPath();
         ctx.fillStyle = 'white';
         ctx.moveTo(gap * i + 6 - this.animark, this.linY);
-        // animark is for animate, the essentially translateX val
         ctx.lineTo(gap * i + 3 + inProptn(0, 9) - this.animark, this.linY);
         ctx.lineTo(gap * i + inProptn(0, 9) - this.animark, this.linY + this.linH);
         ctx.lineTo(gap * i + 1.5 - this.animark, this.linY + this.linH);
@@ -109,6 +92,67 @@ import Litepicker from 'litepicker';
       ppl.draw();
     }
   };
+
+  let signs = {
+    page: 1,
+    pS: [document.querySelector('.page-sign-next'), document.querySelector('.page-sign-prev')],
+    // height is not responsive atm!!
+    get yPos() {
+      return (road.y - (road.linH / 3) * 1.5)-inProptn(1, 6); },
+    draw() {
+      ctx.fillStyle = '#cdc597';
+      ctx.fillRect(init.w - inProptn(0, 21), this.yPos, inProptn(0, 150), inProptn(1, 6));
+      this.pS[0].style.top = `${this.yPos - this.pS[0].clientHeight}px`;
+      this.pS[0].style.left = `${init.w - inProptn(0, 21) - this.pS[0].clientWidth}px`;
+      // next obj init
+      car.position();
+    }
+  };
+
+  let car = {
+    thingItself: document.querySelector('.car'),
+    // a short wee thing but an obj in its own right mostly for anime
+    position() {
+      this.thingItself.style.bottom = `${vp2px(0, 10)}px`;
+      // this! is properly not reponsive
+    }
+  };
+
+  let grass = {
+    amt: inProptn(0, 50),
+    // each grass img is 50x50 so the amt needed 2 fill up screen is w/50
+    roots: new Array(inProptn(0, 50)),
+    // for holding each img
+    soil: new Array(4),
+    // for each line of grass, differently positioned
+    get lawnmower() {
+       Array.from(document.querySelector('.grass-cont').children); },
+      // not sure i 'get' getters yet but not simply a prop nor has a return since both target a representation of arr and not the actual thing as it exists in the dom, maybe what the 'direct' part of getter is except as a method does the exact same, so still seems redundant...
+    draw() {
+      if (this.lawnmower) {
+      // if empty, this is the first init; if true, mow all the old grass and redraw relevant to resize
+        this.lawnmower.forEach(x => {
+          while (x.lastChild) {
+            x.lastChild.remove();
+          }
+        } ); }
+      for (let i = 0; i < 4; i++) {
+        // 4 lines of grass
+        this.soil[i] = document.querySelector(`.grass${i}`);
+        this.soil[i].style.width = `${this.amt * init.w}px`;
+        for (let j = 0; j < this.amt; j++) {
+          this.roots[j] = new Image(50, 50);
+          this.roots[j].src = '../img/grs.png';
+          this.soil[i].appendChild(this.roots[j]);
+        }
+      }
+      this.soil.forEach( x => { x.style.bottom = `${this.soil.indexOf(x) * 30 - 50}px`; } );
+      // all lines just slightly on top of each other n under the road, except for the last
+      this.soil[3].style.top = `${road.y - 50 - (road.linH / 3)}px`;
+    }
+  };
+
+  // _*_*_*_*_*_*_*_*_| Pg. 1 PEOPLE |_*_*_*_*_*_*_*_*_*_
 
   let ppl = {
     cont: document.querySelector('.ppl-cont'),
@@ -124,7 +168,7 @@ import Litepicker from 'litepicker';
         this.chrs[i] = new Image();
         this.chrs[i].src = `../img/${this.picker[i%2]}.png`;
         poke(this.chrs[i]);
-        // adds click events 2 them
+        // poke adds click events 2 them
         this.cont.appendChild(this.chrs[i]);
       }
       signs.draw();
@@ -151,69 +195,25 @@ import Litepicker from 'litepicker';
     });
   }
 
-  let signs = {
-    page: 1,
-    pS: [document.querySelector('.page-sign-next'), document.querySelector('.page-sign-prev')],
-    // height is not responsive atm!!
-    get yPos() {
-      return (road.y - (road.linH / 3) * 1.5)-inProptn(1, 6); },
-    draw() {
-      ctx.fillStyle = '#cdc597';
-      // eek this is just, ratios of screen w/h etc., x has init.w - etc for right, y is top of curbs from road - this h
-      ctx.fillRect(init.w - inProptn(0, 21), this.yPos, inProptn(0, 150), inProptn(1, 6));
-      this.pS[0].style.top = `${this.yPos - this.pS[0].clientHeight}px`;
-      this.pS[0].style.left = `${init.w - inProptn(0, 21) - this.pS[0].clientWidth}px`;
-      // next obj init
-      car.position();
-    }
-  };
+  // _*_*_*_*_*_*_*_*_| Pg. 2 CALENDAR |_*_*_*_*_*_*_*_*_*_
 
-  let car = {
-    thingItself: document.querySelector('.car'),
-    // a short wee beast but useful in other things, like animate n
-    position() {
-      this.thingItself.style.bottom = `${vp2px(0, 15)}px`;
-      // this! is properly not reponsive
-    }
-  };
-
-  let grass = {
-    amt: inProptn(0, 50),
-    // each grass img is 50x50 so the amt needed 2 fill up screen is w/50
-    roots: new Array(inProptn(0, 50)),
-    // for holding each img
-    soil: new Array(4),
-    // for each line of grass, differently positioned
-    get lawnmower() {
-       Array.from(document.querySelector('.grass-cont').children); },
-      // very interesting!! that a return here messes it all up, because then the lawnmower only mows the values of a new arr called lawnmower (and the same without the getter), whereas without the return lawnmower literally is one and the same with the arr of grass, and so can delete the dom elements accordingly
-    draw() {
-      if (this.lawnmower) {
-      // if empty, first init; if true, mow all the old grass and redraw relevant to resize
-        this.lawnmower.forEach(x => {
-          while (x.lastChild) {
-            x.lastChild.remove();
-          }
-        } ); }
-      for (let i = 0; i < 4; i++) {
-        this.soil[i] = document.querySelector(`.grass${i}`);
-        this.soil[i].style.width = `${this.amt * init.w}px`;
-        for (let j = 0; j < this.amt; j++) {
-          this.roots[j] = new Image(50, 50);
-          this.roots[j].src = '../img/grs.png';
-          this.soil[i].appendChild(this.roots[j]);
-        }
-      }
-      this.soil.forEach( x => { x.style.bottom = `${this.soil.indexOf(x) * 30 - 50}px`; } );
-      this.soil[3].style.top = `${road.y - 50 - (road.linH / 3)}px`;
-    }
-  };
+  const picker = new Litepicker({
+    element: document.querySelector('.date'),
+    startDate: new Date(),
+    inlineMode: true,
+    autoRefresh: true,
+    autoApply: true,
+    maxDays: 15
+  });
 
   let date = {
     cont: document.querySelector('.date-cont'),
-    inst: [new Date()],
+    // inst[0] for default day (today), [1] to be for the end of date range (these will change a bit)
+    inst: [new Date(), new Date()],
+    // ms in a day
     msDays: 84600000,
     txtEle: document.querySelector('.human-form-day'),
+    // to present date in a sensible format
     humanFriendly(d) {
       return d.toDateString().slice(0, -4); },
     init() {
@@ -221,7 +221,7 @@ import Litepicker from 'litepicker';
       this.cont.style.left = `${init.w}px`;
       this.cont.style.width = `${picker.ui.clientWidth + 18}px`;
       // i quickly became cssphobic
-      this.txtEle.textContent = `${this.humanFriendly(this.inst[0])} until ${this.humanFriendly(this.inst[0])}`;
+      this.txtEle.textContent = `${this.humanFriendly(this.inst[0])} until ${this.humanFriendly(this.inst[1])}`;
     },
     get dayDiff() {
       return Math.floor(Math.abs(this.inst[1]-this.inst[0])/this.msDays)+1; },
@@ -231,58 +231,95 @@ import Litepicker from 'litepicker';
   date.noEle.addEventListener('click', function(e) {
     let inpMS = date.inst[0].getTime() +
     Math.floor(date.msDays * date.noEle.valueAsNumber) - date.msDays;
+    // just highlighting the daterange for input in the lil up n down no box rather than picker itself
     picker.setDateRange(date.inst[0], new Date(inpMS));
   }, false);
 
   function datedealer() {
     let clkd = picker.getDate();
-    date.inst.push(clkd.dateInstance);
-    date.txtEle.textContent = `${date.humanFriendly(date.inst[0])} until ${date.humanFriendly(date.inst[0])}`;
-    if (date.inst.length > 1) {
-      picker.setDateRange(date.inst[0], date.inst[1]);
-      date.noEle.value = `${date.dayDiff}`;
-      date.days = date.dayDiff;
-      date.txtEle.textContent = `${date.humanFriendly(date.inst[0])} until ${date.humanFriendly(date.inst[1])}`;
-      date.inst.shift();
+    // ensuring the human readable date has the later date last
+    if (clkd.dateInstance > date.inst[0]) {
+      date.inst[1] = clkd.dateInstance;
+    } else {
+      date.inst[0] = clkd.dateInstance;
+    }
+    // highlighting daterange for pickled picked picker dates
+    picker.setDateRange(date.inst[0], date.inst[1]);
+    // letting inp no box reflect that
+    date.noEle.value = `${date.dayDiff}`;
+    date.txtEle.textContent = `${date.humanFriendly(date.inst[0])} until ${date.humanFriendly(date.inst[1])}`;
+  }
+
+  // let valiDate = $('.day-no').parsley();
+  // if (!valiDate.isValid()) {
+  //   window.jQuery.addError(valiDate, {message: })
+  // }
+  // console.log(valiDate.isValid());
+
+  // _*_*_*_*_*_*_*_*_| Pg. 3 CAR SELECT |_*_*_*_*_*_*_*_*_*_
+
+  function chosen1() {
+    for (let i = 0; i < omni.keyz.length; i++) {
+      // for each vechicle, as repped by index in omni.val props, check inputted ppl no is within range, n then the same for days, and for the index that passes the test, push that (i as .keyz index === car) to options arr
+      if (ppl.no >= omni.vals.ppl[i][0] && ppl.no <= omni.vals.ppl[i][1] && date.noEle.valueAsNumber >= omni.vals.days[i][0] && date.noEle.valueAsNumber <= omni.vals.days[i][1]) {
+        mechanic.opts.push(omni.keyz[i]);
+        console.log(mechanic.opts, i);
+      }
     }
   }
 
   let mechanic = {
-    cont: document.querySelector('.v-cont'),
-    vechs: Array.from(document.querySelector('.vechs').children),
-    vRightPos: [12, 7, 81],
-    // fyi arr order of vechs is mb, mn, cpv
+    txt: document.querySelector('.v-tit'),
+    caryard: document.querySelector('.vechs'),
+    opts: [],
+    clkd: [],
     anicar: 0,
-    // carFacts: ['']
     init() {
-      this.cont.style.left = `${init.w * 2}px`;
-      for (let i = 0; i < this.vechs.length; i++) {
-        // this.vechs[i].style.right = `${vp2px(this.vechs[i])}`
-        this.vechs[i].style.right = `${vp2px(1, this.vRightPos[i]) - init.w + this.anicar}px`;
-        // can u rly not get right pos from dom?
+      this.txt.style.left = `${init.w * 2}px`;
+      this.caryard.style.left = `${init.w * 2 + this.anicar}px`;
+    },
+    present() {
+      // since must(ang) alr on screen as default, a. don't wanna try add it again and b. can drive offscreen into the abyss
+      if (this.opts.includes('must')) {
+        this.opts = this.opts.filter(x => x !== 'must');
+      } else {
+        anime({
+          targets: car.thingItself,
+          translateX: init.w,
+          easing: 'easeOutQuad',
+          duration: 1600
+        });
+        window.setTimeout(function() {
+          car.thingItself.classList.add('hide');
+        }, 1400);
       }
+      this.opts.forEach(x => {
+        console.log(x);
+        let c = new Image();
+        c.src = `../img/${x}.png`;
+        c.classList.add('v', `${x}`);
+        this.caryard.appendChild(c);
+        carChat();
+      });
+      window.clearTimeout();
+      // not strictly necessary but good practice ig
     }
   };
 
-  mechanic.vechs.forEach(x => {
-    x.addEventListener('click', function(e) {
-      let name = x.classList[1];
-      let newD = document.createElement('div');
-      newD.classList.add('wrapper', 'block', `${name}`, 'manu-facts');
-      document.querySelector('.v-info').appendChild(newD);
-      cardealer();
-    }, true);
-  });
-
-  function cardealer() {
-
+  function carChat() {
+    // tog 2 hide info on second click
+    let tog = 0;
+    Array.from(mechanic.caryard.children).forEach(x => {
+      x.addEventListener('click', function(e) {
+        tog++;
+        let newD = document.createElement('div');
+        newD.classList.add('wrapper', 'block', `${mechanic.clkd[mechanic.clkd.length - 1]}`, 'manu-facts');
+        document.querySelector('.v-info').appendChild(newD);
+      }, false);
+    });
   }
 
-  console.log(omni.mb.get('ppl'), Object.keys(omni));
-
-  Object.keys.forEach(x => {
-    console.log(x);
-  })
+  // _*_*_*_*_*_*_*_*_| HANDY DANDY FUNCS |_*_*_*_*_*_*_*_*_*_
 
   function animate() {
     anime({
@@ -316,8 +353,10 @@ import Litepicker from 'litepicker';
       duration: 2700
     });
     if (signs.page === 2) {
+      chosen1();
+      mechanic.present();
       anime({
-        targets: mechanic,
+        targets: mechanic.caryard,
         anicar: init.w,
         round: 1,
         easing: 'easeOutExpo',
@@ -441,12 +480,14 @@ import Litepicker from 'litepicker';
    function vp2px(d, vw) {
     // d T = vw/h -> px, d F reverse
     return d ? Math.round((window.innerHeight * vw) / 100)
-      : Math.round(((vw * 100) / window.innerWidth) * 100);
-      // this second one a bit diff from the formula on stackoverflow but f it
+      : Math.round((vw * 100 / window.innerWidth) * 100);
   }
 
   signs.pS[0].addEventListener('click', animate, false);
   picker.ui.addEventListener('click', datedealer, false);
+
+  // cache was annoying
+  date.noEle.value = 1;
 
   'load resize'.split(' ').forEach(function(e) {
     // rescale things on both page load n resize
